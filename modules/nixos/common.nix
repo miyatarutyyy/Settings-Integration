6a57a66b741e9517611965e410f850fe17328e94
miyatarutyyy <study.miyata026@gmail.com>
2026-07-25 14:01:19 +0900

    add common nixos base settings

---
+++ modules/nixos/common.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  pkgs,
+  username,
+  ...
+}:
+
+{
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  hardware.enableRedistributableFirmware = lib.mkDefault true;
+
+  time.timeZone = "Asia/Tokyo";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  users.users.${username} = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "dialout"
+    ];
+    shell = pkgs.bashInteractive;
+  };
+
+  programs.bash.enableCompletion = true;
+
+  home-manager.backupFileExtension = "hm-backup";
+
+  environment.systemPackages = with pkgs; [
+    curl
+    git
+    vim
+    wget
+  ];
+
+  networking.useDHCP = lib.mkDefault true;
+  networking.wireless.iwd = {
+    enable = lib.mkDefault true;
+    settings.Settings.AutoConnect = true;
+  };
+
+  services.openssh.enable = lib.mkDefault true;
+}
