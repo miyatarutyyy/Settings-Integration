72ba484ceb6ef4f46399a2e0a18332f67fb2cd95
miyatarutyyy <study.miyata026@gmail.com>
2026-07-25 16:46:26 +0900

    Add NixOS desktop runtime integration

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -7,6 +7,8 @@
 {
   programs.niri.enable = true;
 
+  services.dbus.enable = true;
+
   services.greetd = {
     enable = true;
     useTextGreeter = true;
@@ -25,5 +27,55 @@
         '';
       };
     };
+  };
+
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+    ];
+
+    config = {
+      common = {
+        default = [
+          "gnome"
+          "gtk"
+        ];
+        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+      };
+
+      niri = {
+        default = [
+          "gnome"
+          "gtk"
+        ];
+        "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+      };
+    };
+  };
+
+  services.gnome.gnome-keyring.enable = true;
+
+  security.pam.services = {
+    greetd.enableGnomeKeyring = true;
+    hyprlock = { };
   };
 }
