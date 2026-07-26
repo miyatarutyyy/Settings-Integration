8980be2a077aeeef6808b96f2ee2231213622504
miyatarutyyy <study.miyata026@gmail.com>
2026-07-26 14:50:14 +0900

    Add common GUI app packages

--- home/common.nix
+++ home/common.nix
@@ -7,6 +7,7 @@
     ./modules/desktop.nix
     ./modules/development.nix
     ./modules/emacs.nix
+    ./modules/gui-apps.nix
     ./modules/git.nix
     ./modules/idle-lock.nix
     ./modules/shell.nix
---
+++ home/modules/gui-apps.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    floorp-bin
+    discord
+    mpv
+    pwvucontrol
+  ];
+}
